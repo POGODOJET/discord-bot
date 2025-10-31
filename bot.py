09eb5451b28aaafc2ad724b3a20606972c9d0e42
POGODOJET <114024155+POGODOJET@users.noreply.github.com>
2025-10-31 02:14:34 -0300

    Fix color code in embed for announcement command

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ async def anuncio(ctx):
     embed = discord.Embed(
         title="🟪 PAINEL:",
         description="[https://heavencity.com/](https://heavencity.com/)",
-        color=A02D8E  # Roxo
+        color=0xA02D8E  # Roxo
     )
 
     embed.add_field(
@@ -40,5 +40,6 @@ async def anuncio(ctx):
 # Token seguro vindo das variáveis da Railway
 TOKEN = os.getenv("DISCORD_TOKEN")
 bot.run(TOKEN)
+
 
 
