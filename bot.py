071e3b41edd78189c8c95ff24b77ee65cb26e029
POGODOJET <114024155+POGODOJET@users.noreply.github.com>
2025-11-09 21:41:52 -0300

    Refactor on_ready event to suppress errors
    Removed error logging in the on_ready event.

--- bot.py
+++ bot.py
@@ -3,39 +3,31 @@ import os
 from discord.ext import commands
 import discord
 
-
 from tickets import setup_tickets
 from comandos import setup_commands
-
 
 # IDs fornecidos
 CATEGORY_ID = 1387269436259434557
 LOG_CHANNEL_ID = 1436234566015914077
 STAFF_ROLE_ID = 1387269134609420358
 
-
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 
 # setup modules
 setup_tickets(bot, CATEGORY_ID, LOG_CHANNEL_ID, STAFF_ROLE_ID)
 setup_commands(bot)
-
 
 @bot.event
 async def on_ready():
     print(f"✅ Bot conectado como {bot.user}")
     try:
         await bot.tree.sync()
-        print("✅ Comandos sincronizados!")
-    except Exception as e:
-        print(f"❌ Erro ao sincronizar comandos: {e}")
-
+    except Exception:
+        pass
 
 if __name__ == "__main__":
     TOKEN = os.getenv("DISCORD_TOKEN")
